71b2b1fe66addbdb0c0370f028b40c0f824888f9
Linque <1152350495@qq.com>
2023-08-20 20:06:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+    <h1>我是app根组件</h1>
+  </div>
+</template>
+<script setup lang="ts">
+const str = 'asdsadasd'
+console.log(str)
+</script>
+<style lang="scss" scoped>
+.box {
+  width: 200px;
+}
+</style>
